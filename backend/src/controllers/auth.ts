28e1dca8e31930e758b3f15092f81452130438a1
basel2053 <baselsalah2053@gmail.com>
2023-05-17 13:06:23 +0300

    feat: adding signup controller to create new accounts for job seekers

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -7,23 +7,20 @@ import knex from './../config/db-conf';
 export const signup = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  // IMPORTANT  knex in where clause when getting value of undefined throws an error
-  // NOTE  HERE  make it possible to work with lower or upper case (not case sensitive)
-  const existingUser = await knex.select('*').from('user_account').where('email', email).first();
-
-  const hashedPassword = await bcrypt.hash(password, 8);
+  // IMPORTANT  knex in where clause when getting value of undefined throws an error (use validator)
+  const existingUser = await knex.select('*').from('user_account').where('email', 'ILIKE', email).first();
 
   if (existingUser) {
     throw new Error('nope, this email is already used buddy');
   }
 
-  // NOTE  remove password from the return
-  const user = await knex.insert({ email, password: hashedPassword }).into('user_account').returning('*');
+  const hashedPassword = await bcrypt.hash(password, 8);
 
-  const token = jwt.sign({ id: user[0].id, email: user[0].email }, 'secret');
-  console.log(token);
+  const user = await knex.insert({ email, password: hashedPassword }).into('user_account').returning(['email', 'id']);
 
-  res.status(201).send(user[0]);
+  const accessToken = jwt.sign({ id: user[0].id, email: user[0].email }, 'secret');
+
+  res.status(201).send({ user: user[0], accessToken });
 };
 
 // export const login = (req: Request, res: Response) => {};
