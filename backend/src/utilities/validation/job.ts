b51f80818b9895b6fbb0623f4a526e4249901b17
basel2053 <baselsalah2053@gmail.com>
2023-05-20 16:54:20 +0300

    feat: adding job validation

---
+++ backend/src/utilities/validation/job.ts
@@ -0,0 +1,25 @@
+import joi from 'joi';
+
+export const jobCreateSchema = joi.object({
+  title: joi.string().max(200).required(),
+  description: joi.string().max(600).required(),
+  type: joi.string().valid('Part-time', 'Full-time').required(),
+  location: joi.string().required(),
+  min_salary: joi.number().integer().positive(),
+  max_salary: joi.number().integer().positive(),
+  experience: joi.string().max(100),
+  skills: joi.array().items(joi.string()).min(2).max(30),
+});
+
+export const jobUpdateSchema = joi
+  .object({
+    title: joi.string().max(200).required(),
+    description: joi.string().max(600).required(),
+    type: joi.string().valid('Part-time', 'Full-time').required(),
+    location: joi.string().required(),
+    min_salary: joi.number().integer().positive(),
+    max_salary: joi.number().integer().positive(),
+    experience: joi.string().max(100),
+    skills: joi.array().items(joi.string()).min(2).max(30),
+  })
+  .min(1);
