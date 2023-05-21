1827c2d978070c9190a5fa216e0836c55cb97c73
basel2053 <baselsalah2053@gmail.com>
2023-05-21 15:10:53 +0300

    refactor: checking roles of users to manage routes access

--- backend/src/routes/v1/employer.ts
+++ backend/src/routes/v1/employer.ts
@@ -4,6 +4,7 @@ import { signup, show, update } from '../../controllers/employer';
 import { requireAuth } from '../../middlewares/requireAuth';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { employerSignupSchema, employerUpdateSchema } from '../../utilities/validation/employer';
+import { checkRole } from '../../middlewares/checkRole';
 
 const router = Router();
 
@@ -11,6 +12,6 @@ router.get('/employers/:id', requireAuth, show);
 
 router.post('/employers', validateRequest(employerSignupSchema), signup);
 
-router.patch('/employers', requireAuth, validateRequest(employerUpdateSchema), update);
+router.patch('/employers', requireAuth, checkRole('employer'), validateRequest(employerUpdateSchema), update);
 
 export { router as employerRouter };
