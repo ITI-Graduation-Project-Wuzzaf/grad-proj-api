eecfe3149002c449c4f8f1ba19bef32205a0738f
basel2053 <baselsalah2053@gmail.com>
2023-05-29 00:08:47 +0300

    refactor: adding account rate limiter

--- backend/src/middlewares/rateLimit.ts
+++ backend/src/middlewares/rateLimit.ts
@@ -6,4 +6,19 @@ export const limiter = (maxRequests: number) =>
     max: maxRequests,
     standardHeaders: true,
     legacyHeaders: false,
+    message: 'Too many requests sent from this IP, please try again later.',
   });
+
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Too many login attempts, please try again later.',
+  skipSuccessfulRequests: true,
+  keyGenerator: (_req, res) => {
+    console.log(res.locals.userId + res.locals.role);
+
+    return res.locals.userId + res.locals.role;
+  },
+});
