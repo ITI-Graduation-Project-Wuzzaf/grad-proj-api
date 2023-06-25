d961f15b5ab4b72ecd590b83399b72d3fbb40b3a
basel2053 <baselsalah2053@gmail.com>
2023-06-25 03:24:31 +0300

    changing stripe redirect origin

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -34,8 +34,8 @@ export const checkout = async (req: Request, res: Response) => {
         quantity: 1,
       },
     ],
-    success_url: 'http://localhost:5173/',
-    cancel_url: 'http://localhost:5173/?payment=failed',
+    success_url: 'https://jobify-ruby-one.vercel.app/',
+    cancel_url: 'https://jobify-ruby-one.vercel.app/?payment=failed',
   });
 
   res.send({ sessionId: session.id });
